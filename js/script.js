49b97efccc06e5dd0581439af7776868061a7c3a
mattia.rollo <ste.tia@hotmail.it>
2022-10-07 17:32:50 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -7,9 +7,21 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65.
 
-creao una variabile per registrare i km inseriti dall'utente
-creo una vaiabile per gli anni inseriti dall'utente
+creare una variabile per registrare i km inseriti dall'utente
+creare una vaiabile per gli anni inseriti dall'utente
 li converto in numeri
+creare una variabile che misura i km con il prezzo pee km 
+creare variabili costanti per under18 e over 65
+creare due variabili che mi calcolano lo sconto in base al prezzo e l'eta
+creare un messaggio che inizialmente mi dice il prezzo
+
+prendere l'elemento bottone con classe calcola 
+applicare la funzione che mi cattura l'evento e avvia una funzione 
+
+creare una funzione che
+    se l'utente è minore di 18 mi applica lo sconto del 20% e mi scrive un messaggio che è stato applicato lo sconto
+    se l'utente è maggiore di 65  applica lo sconto del 40 % e scrive un messaggio  
+
 
 
 
@@ -39,7 +51,7 @@ const priceTicket = function () {
     const over65 = 65;
     const scontoUnder18 = price * 0.2;
     const scontoOver65 = price * 0.4;
-    let message = `il prezzo del biglietto è di: ${price} €`
+    let message = `Il prezzo del biglietto è di: ${price} €`
 
     const myDiv = document.getElementById('messaggio');
 
